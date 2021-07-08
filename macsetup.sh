ca6a482ab756ece8b23eaa53d0c938ae13825dfc
Fabian Gunzinger <fa.gunzinger@gmail.com>
2021-07-08 07:20:43 +0100

    add miniconda install

--- macsetup.sh
+++ macsetup.sh
@@ -4,3 +4,7 @@ brew tap hashicorp/tap
 brew install hashicorp/tap/terraform
 
 
+
+# miniconda install (check that it contains python version you want)
+curl -O https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh
+bash Miniconda3-latest-MacOSX-x86_64.sh
