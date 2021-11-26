930b0aa9e6bcd562598595036f30557b36fe9991
Fabian Gunzinger <fa.gunzinger@gmail.com>
2021-11-26 19:46:20 +0000

    fzf install

--- macsetup.sh
+++ macsetup.sh
@@ -9,6 +9,12 @@ brew install ruby
 
 
 
+# tools
+
+brew install fzf
+$(brew --prefix)/opt/fzf/install
+
 # miniconda install (check that it contains python version you want)
 curl -O https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh
 bash Miniconda3-latest-MacOSX-x86_64.sh
+rm Miniconda3-latest-MacOSX-x86_64.sh
